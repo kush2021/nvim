0aada0ace7ab2743879c7b94785b0d21bb39ce3c
Kush <kushpadalia2002@gmail.com>
2024-05-17 17:11:56 -0600

    Added Markdown Preview

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -45,4 +45,19 @@ return {
       },
     },
   },
+
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = {
+      "MarkdownPreviewToggle",
+      "MarkdownPreview",
+      "MarkdownPreviewStop",
+    },
+    ft = {
+      "markdown",
+    },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
 }
