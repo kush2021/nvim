29c694092d304459c5a4719a07bb7e01becc6661
Kush <kushpadalia2002@gmail.com>
2024-10-25 21:17:04 -0600

    Added Python Configuration

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,14 @@
+local conform = require "conform"
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
     cpp = { "clang-format" },
-    python = { "black" },
+    python = { "yapf" },
   },
-
   format_on_save = {
     -- These options will be passed to conform.format()
     timeout_ms = 500,
     lsp_fallback = true,
   },
 }
-
-return options
+conform.setup {}
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,8 +1,10 @@
 return {
   {
     "stevearc/conform.nvim",
-    event = "BufWritePre", -- uncomment for format on save
-    opts = require "configs.conform",
+    event = "BufWritePre",
+    config = function()
+      require "configs.conform"
+    end,
   },
 
   -- These are some examples, uncomment them if you want to see them work!
